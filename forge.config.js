1689c849cddbf8ce4db4847dc6442630db240860
tantan.li <tantan.li@hiscene.com>
2023-03-27 16:34:02 +0800

    更新配置文件

--- forge.config.js
+++ forge.config.js
@@ -11,10 +11,15 @@ module.exports = {
     },
   ],
   packagerConfig: {
-    name: 'Electron切换音频输出设备',
-    executableName: 'App',
+    name: 'electron-audio-device-switch',
+    executableName: 'electron-audio-device-switch',
     extraResource: ['./song.mp3'], // 静态文件
     // icon: './build/icon' // 不用加后缀，但是需要准备3个文件，win: icon.ico, mac: icon.icns, linux: icon.png，打包时自动识别，linux 在BrowserWindow构造参数中设置
+    win32metadata: {
+      ProductName: 'Js切换音频输入设备 Windows',
+      CompanyName: 'mypanda.github.com',
+      FileDescription: 'Js切换音频输入设备 for mypanda'
+    }
   },
   rebuildConfig: {},
   makers: [
