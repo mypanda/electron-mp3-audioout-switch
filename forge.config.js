b99bf87e751bb9d8be2350d133a58ad61093703b
tantan.li <tantan.li@hiscene.com>
2023-03-27 16:10:35 +0800

    修改配置文件

--- forge.config.js
+++ forge.config.js
@@ -6,13 +6,16 @@ module.exports = {
         repository: {
           owner: 'mypanda',
           name: 'electron-mp3-audioout-switch',
-        },
-        prerelease: false,
-        draft: true,
+        }
       },
     },
   ],
-  packagerConfig: {},
+  packagerConfig: {
+    name: 'Electron切换音频输出设备',
+    executableName: 'App',
+    extraResource: ['./song.mp3'], // 静态文件
+    // icon: './build/icon' // 不用加后缀，但是需要准备3个文件，win: icon.ico, mac: icon.icns, linux: icon.png，打包时自动识别，linux 在BrowserWindow构造参数中设置
+  },
   rebuildConfig: {},
   makers: [
     {
